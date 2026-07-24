cc04f49de040f3083ae4ea52ed7dd85f41a15487
Priyanka <priyankamm.396@gmail.com>
2026-07-24 11:24:13 +0530

    Structure of the project

---
+++ frontend/src/landing_page/pricing/PricingPage.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Hero from './Hero';
+import OpenAccount from '../OpenAccount';
+import Brockerage from './Brockerage';
+
+function PricingPage() {
+    return ( 
+        <>
+            <Hero />
+            <OpenAccount />
+            <Brockerage />
+        </>
+     );
+}
+
+export default PricingPage;
